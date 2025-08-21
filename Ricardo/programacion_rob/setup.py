26b4e54774f291e125d371dd688f55b98f3ad2af
777Richi <cristian.lizarazo@uao.edu.co>
2025-08-21 15:55:28 -0500

    Creación de carpetas

---
+++ Ricardo/programacion_rob/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'programacion_rob'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='richilzz',
+    maintainer_email='cristian.lizarazo@uao.edu.co',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
