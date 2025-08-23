380adc3c0e357c2b0a0b9324bc8830d96eddc858
Juan José Tovar <juan_jose.tovar@uao.edu.co>
2025-08-23 18:11:38 -0500

    Se añaden parametros para no modificar el código al momento de insertar IP y puerto, segunda capa de verificación de conexión cada 3 minutos, reducción y corrección en la función keepalive

--- Juan_Tovar/pc_to_espcomm.py
+++ Juan_Tovar/pc_to_espcomm.py
@@ -13,8 +13,13 @@ class DegreesSubscriber(Node):
         super().__init__('degree_subscriber')
         
         # TCP connection parameters
-        self.esp32_ip = "0.0.0.0"  # IP de la ESP
-        self.esp32_port = 0000     # Puerto de conexión
+        self.declare_parameter('esp32_ip', '192.168.1.100')
+        self.declare_parameter('esp32_port', 8080)
+        self.esp32_ip = self.get_parameter('esp32_ip').get_parameter_value().string_value
+        self.esp32_port = self.get_parameter('esp32_port').get_parameter_value().integer_value
+        self.get_logger().info(f'Parámetros configurados:')
+        self.get_logger().info(f'ESP32 IP: {self.esp32_ip}')
+        self.get_logger().info(f'ESP32 Port: {self.esp32_port}')
         self.socket = None
         self.connection_lock = threading.Lock()
         
@@ -24,11 +29,18 @@ class DegreesSubscriber(Node):
             self.listener_callback,
             10)
         
-        # Conectar a ESP
-        self.conectar_a_esp32()
+        #Verificación de conexión al inicializar
+        self.get_logger().info('Iniciando conexión a ESP')
+        if not self.conectar_a_esp32():
+            self.get_logger().error('No se pudo conectar a ESP')
+            self.get_logger().error('Verifique que ESP32 esté encendido y en la red')
+            raise RuntimeError(f'No se pudo conectar a ESP en {self.esp32_ip}:{self.esp32_port}')
+        
+        #Revisión de conexión periodica
+        self.timer = self.create_timer(180.0, self.verificar_conexión)
         
         # Función para checkear la conexión periodicamente
-    def set_keepalive(self, sock, after_idle_sec=180, interval_sec=30, max_fails=3):
+    def set_keepalive(self, sock, after_idle_sec=60, interval_sec=7, max_fails=3):
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) # Activa la función del socket que permite el checkeo
             sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec) # Tiempo a esperar antes del checkeo 
             sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec) # Cada cuanto checkea
@@ -41,14 +53,20 @@ class DegreesSubscriber(Node):
                 if self.socket:
                     self.socket.close()
                 
+                self.get_logger().info(f'Conectando a ESP32 en {self.esp32_ip}:{self.esp32_port}...')
                 self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Se define el tipo de socket a usar, estos son los predeterminados y supuestamente los que mejor funcionan xd
-                self.socket.settimeout(10.0) # Tiempo de espera de conexión
+                self.socket.settimeout(5.0) # Tiempo de espera de conexión
                 self.socket.connect((self.esp32_ip, self.esp32_port))
+                # Configuración del socket
+                self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Esta función deshabilita una función de TCP que busca enviar múltiples paquetes en uno solo                
+                self.set_keepalive(self.socket)
                 self.get_logger().info(f'Conectado con ESP32 en {self.esp32_ip}:{self.esp32_port}')
+                return True
                 
         except Exception as e:
             self.get_logger().error(f'Fallo al conectarse con ESP32: {e}')
             self.socket = None
+            return False
             
     def enviar_grados_a_esp32(self, float_list):
         if not self.socket:
@@ -67,23 +85,9 @@ class DegreesSubscriber(Node):
                 format_string = f'B{num_floats}f'
                 packed_data = struct.pack(format_string, num_floats, *float_list)
                 
-                # Configuración del socket
-                self.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)  # Esta función deshabilita una función de TCP que busca enviar múltiples paquetes en uno solo
-                self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)  # Verifica cada cierto tiempo el estado de la conexión
-                
                 # Ponemos un timeout
                 self.socket.settimeout(2.0)
                 self.socket.sendall(packed_data)
-                
-                # O ponemos a que la ESP nos envie  confirmaciones manejando el timeout en ella
-                try:
-                    self.socket.settimeout(0.5)  # Acknowledge rapido
-                    ack = self.socket.recv(1)
-                    if ack != b'K':  #ESP envia un caracter para un acknowledge erroneo
-                        self.get_logger().warn('ESP32 fallo en el acknowledge')
-                except socket.timeout:
-                    # ACK timeout
-                    self.get_logger().debug('ACK timeout')
                 
                 return True
                 
@@ -98,6 +102,13 @@ class DegreesSubscriber(Node):
         except Exception as e:
             self.get_logger().error(f'Error enviando los datos: {e}')
             return False
+    
+    def verificar_conexión(self):
+        if not self.socket:
+            self.get_logger().error('Intentando reconectar')
+            self.conectar_a_esp32()
+        else:
+            self.get_logger().info('Conexión activa')
             
     def listener_callback(self, msg):
         self.get_logger().info('Grados recibidos: "%s"' % msg.data)
